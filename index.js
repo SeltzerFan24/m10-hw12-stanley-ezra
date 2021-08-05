0c411994664df7b4322d4a40de6fd0fd284f03ca
Ezra Stanley <ezrastanley09@gmail.com>
2021-08-05 17:32:49 -0400

    create car class

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-// YOUR CODE HERE
+class Car {
+    constructor(make, model, year) {
+        this.make = make;
+        this.model = model;
+        this.year = year;
+    }
+
+    honk () {
+        console.log('BEEP BEEP!');
+    }
+
+    performMaintenance () {
+        setTimeout(() => {
+            console.log('maintenance complete')
+        }, 3000)
+    }
+}
+
+let mySweetRide = new Car('mySweetRide')
